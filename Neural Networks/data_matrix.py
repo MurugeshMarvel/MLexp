e76aa773fcbed06cde1993d5ed3519b1ebac29f5
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-02-01 16:52:42 +0530

    changed the data collection method

--- Neural Networks/data_matrix.py
+++ Neural Networks/data_matrix.py
@@ -1,3 +1,7 @@
+
+import nltk
+import itertools
+import csv
 Voc_size = 8000
 unknown_token = "UNKNOWN_TOKEN"
 sentence_start_token = "SENTENCE_START"
@@ -6,7 +10,9 @@ sentence_end_token = "SENTENCE_END"
 
 '''reading data'''
 print "Reading CSV file...."
-with open('data/comment-2015-08.csv','rb') as f:
+with open('comments-2015-08.csv','rb') as f:
 	reader = csv.reader(f, skipinitialspace = True)
 	reader.next()
-	sentences = itertools.
+	sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower())for x in reader])
+	sentences = ["%s%s%s" % (sentence_start_token,x,sentence_end_token)for x in sentences]
+print "Parsed %d sentences" % (len(sentences))
