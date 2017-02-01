5a47be19676dd0cd725c84d09d849bee2d5b9ce1
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-02-01 16:35:03 +0530

    added RNN data matrix model

---
+++ Neural Networks/data_matrix.py
@@ -0,0 +1,9 @@
+Voc_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+
+'''reading data'''
+print "Reading CSV file...."
+with open('data/comment-2015-08.csv')
