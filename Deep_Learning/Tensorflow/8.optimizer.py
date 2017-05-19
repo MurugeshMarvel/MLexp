4228069a1db2597da4cc82bb7824ef5f167f2ee7
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-05-19 22:27:32 +0530

    working on optimizer

--- Deep_Learning/Tensorflow/8.optimizer.py
+++ Deep_Learning/Tensorflow/8.optimizer.py
@@ -10,3 +10,13 @@ BATCH_SIZE = 32
 
 x = np.linspace(-1,1,100)[:,np.newaxis]
 noise = np.random.normal(0, 0.1, size=x.shape)
+y = np.power(x,2) + noise
+
+plt.scatter(x,y )
+plt.show()
+
+class Net:
+	def __init__(self, opt, **kwargs):
+		self.x = tf.placeholder(tf.float32, [None,1])
+		self.y = tf.placeholder(tf.float32, [None, 1])
+		l = tf.layers.dense(self.x, 20, tf.nn.relu)
