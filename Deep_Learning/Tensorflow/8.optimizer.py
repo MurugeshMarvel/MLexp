1cb8291842bb86bfc01937605500a866357841b0
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-05-20 10:22:00 +0530

    optimizer

--- Deep_Learning/Tensorflow/8.optimizer.py
+++ Deep_Learning/Tensorflow/8.optimizer.py
@@ -20,3 +20,38 @@ class Net:
 		self.x = tf.placeholder(tf.float32, [None,1])
 		self.y = tf.placeholder(tf.float32, [None, 1])
 		l = tf.layers.dense(self.x, 20, tf.nn.relu)
+		out = tf.layers.dense(l,1)
+		self.loss = tf.losses.mean_squared_error(self.y, out)
+		self.train = opt(LR, **kwargs). minimize(self.loss)
+
+
+net_SGD = Net(tf.train.GradientDescentOptimizer)
+net_Momentum = Net(tf.train.MomentumOptimizer, momentum = 0.9)
+net_RMSprop = Net(tf.train.RMSPropOptimizer)
+net_Adam = Net(tf.train.AdamOptimizer)
+nets = [net_SGD, net_Momentum, net_RMSprop, net_Adam]
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+losses_his = [[],[],[],[]]
+
+for step in range(300):
+	index = np.random.randint(0, x.shape[0],BATCH_SIZE)
+	b_x = x[index]
+	b_y = y[index]
+
+
+	for net, l_his in zip(nets, losses_his):
+		_, l = sess.run([net.train, net.loss], {net.x: b_x, net.y:b_y})
+		l_his.append(l)
+
+labels = ['SGD','Momentum','RMSprop','Adam']
+for i,l_his in enumerate(losses_his):
+	plt.plot(l_his, label=labels[i])
+
+plt.legend(loc='best')
+plt.xlabel('steps')
+plt.ylabel('loss')
+plt.ylim((0,0.2))
+plt.show()
